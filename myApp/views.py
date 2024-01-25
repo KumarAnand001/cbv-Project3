4fed736f269614c46dce97e9eeb7f9769f51b511
KumarAnand001 <anandkumarjnu9@gmail.com>
2024-01-25 11:56:07 +0530

    first commit

---
+++ myApp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from myApp.models import Company
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+# Create your views here.
+class CompanyListView(ListView):
+
+    model = Company
+
+class CompanyDetailView(DetailView):
+
+    model = Company
+
+class CompanyCreateView(CreateView):
+
+    model = Company
+    fields = ['name', 'location', 'ceo']
+
+class CompanyUpdateView(UpdateView):
+
+    model = Company
+    fields = ['name', 'ceo']
